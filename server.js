0e58abbb55e1266c32ca3ea6baa1b6d13925adf3
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-24 23:42:40 -0400

    additional middleware

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const { PORT } = process.env;
 const routes = require('./routes/index')
+const cors = require("cors")
 
-// app.get("/", (req, res) => {
-//     res.send("test route") // working
-// })
-
+app.use(cors())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use('/', routes)
 app.use((req, res) => {res.status(404).json({message: "Page does not exist."})})
 
