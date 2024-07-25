2d46634c9d9824c0b3e7a5c80885a3ec8614b9b0
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-24 21:16:23 -0400

    mongodb connection

--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
+const { DATABASE_URL } = process.env;
 
+async function connectToMongo() {
+  try {
+    await mongoose.connect(DATABASE_URL);
+    console.log("Connected to mongodb");
+  } catch (err) {
+    console.error("Connection error: ", err.message);
+  }
+}
+
+connectToMongo();
